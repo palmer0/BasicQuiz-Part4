d1fd26c211bc5eefca6d7af83efec35c3e1e2662
Luis Hernandez <lhdez@iuma.ulpgc.es>
2020-03-05 09:21:17 +0000

    mejorando el codigo

--- app/src/main/java/es/ulpgc/eite/da/basicquizlab/QuestionActivity.java
+++ app/src/main/java/es/ulpgc/eite/da/basicquizlab/QuestionActivity.java
@@ -123,24 +123,28 @@ public class QuestionActivity extends AppCompatActivity {
         trueButtonClicked();
     }
 
+    updateLayoutContent();
+
     nextButtonEnabled = true;
   }
 
   private void trueButtonClicked() {
 
     if(replyArray[questionIndex] == 1) {
-      replyText.setText(R.string.correct_text);
+      currentReply=getString(R.string.correct_text);
     } else {
-      replyText.setText(R.string.incorrect_text);
+      currentReply=getString(R.string.incorrect_text);
     }
+
+
   }
 
   private void falseButtonClicked() {
 
     if(replyArray[questionIndex] == 0) {
-      replyText.setText(R.string.correct_text);
+      currentReply=getString(R.string.correct_text);
     } else {
-      replyText.setText(R.string.incorrect_text);
+      currentReply=getString(R.string.incorrect_text);
     }
 
   }
@@ -164,8 +168,9 @@ public class QuestionActivity extends AppCompatActivity {
     if (requestCode == CHEAT_REQUEST) {
       if (resultCode == RESULT_OK) {
 
-        boolean answerCheated =
-            data.getBooleanExtra(CheatActivity.EXTRA_CHEATED, false);
+        boolean answerCheated = data.getBooleanExtra(
+            CheatActivity.EXTRA_CHEATED, false
+        );
 
         Log.d(TAG, "answerCheated: " + answerCheated);
 
